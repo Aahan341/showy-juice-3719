9cbc53cdbf75244cc611af52630153ea591627bb
Aahan341 <aaahan006@gmail.com>
2023-01-19 01:27:38 +0530

    fp06_244-day-2

---
+++ reparation-club/src/App.js
@@ -0,0 +1,15 @@
+
+import TopNavbar from "./Components/TopNavbar";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TopNavbar/>
+     {/* <Navbar/> */}
+    
+    </div>
+  );
+}
+
+export default App;
